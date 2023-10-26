2350b5dd3b3f7cbe2fcc829944c93d9b3fc3af2e
Kingsley Ogbonna <kingsleyogbonna0gmail.com>
2023-10-27 01:50:57 +0300

    fairy tale app challenge

---
+++ FairyTalesDemo/MainPage.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections.ObjectModel;
+using FairyTalesDemo.Models;
+using Microsoft.Maui.Handlers;
+
+namespace FairyTalesDemo;
+
+public partial class MainPage : ContentPage
+{
+	public ObservableCollection<FairyTale> FairyTales { get; set; }
+
+    public ObservableCollection<FairyTale> FairyTales2 { get; set; }
+
+
+
+    public MainPage()
+	{
+		InitializeComponent();
+		ModifySearchBar();
+		InitializeTales();
+
+		BindingContext = this;
+	}
+
+    private void InitializeTales()
+    {
+        FairyTales = new ObservableCollection<FairyTale>
+               {
+                   new FairyTale { Name = "Cinderella", ReadTime = new TimeSpan(0, 30, 0), Image = "cinderella.jpg" },
+                   new FairyTale { Name = "Snow White", ReadTime = new TimeSpan(0, 25, 0),  Image = "snow.jpg" },
+                   new FairyTale { Name = "Rapunzel", ReadTime = new TimeSpan(0, 20, 0),  Image = "rapunzel.jpg" },
+                   new FairyTale { Name = "Little Red Riding Hood", ReadTime = new TimeSpan(0, 15, 0),  Image = "hood.jpg" },
+                   new FairyTale { Name = "Beauty and the Beast", ReadTime = new TimeSpan(0, 35, 0),  Image = "beauty.jpg" }
+               };
+        FairyTales2 = new ObservableCollection<FairyTale>
+               {
+                   new FairyTale { Name = "Snow White", ReadTime = new TimeSpan(0, 25, 0),  Image = "snow.jpg" },
+                   new FairyTale { Name = "Rapunzel", ReadTime = new TimeSpan(0, 20, 0),  Image = "rapunzel.jpg" },
+                   new FairyTale { Name = "Little Red Riding Hood", ReadTime = new TimeSpan(0, 15, 0),  Image = "hood.jpg" },
+                   new FairyTale { Name = "Beauty and the Beast", ReadTime = new TimeSpan(0, 35, 0),  Image = "beauty.jpg" },
+                   new FairyTale { Name = "Cinderella", ReadTime = new TimeSpan(0, 30, 0),  Image = "cinderella.jpg" }
+               };
+    }
+
+    private void ModifySearchBar()
+	{
+		SearchBarHandler.Mapper.AppendToMapping("CustomSearchIconColor", (handler, view) =>
+		{
+#if ANDROID
+			var context = handler.PlatformView.Context;
+			var searchIconId = context.Resources.GetIdentifier("search_mag_icon", "id", context.PackageName);
+			if(searchIconId != 0)
+			{
+				var searchIcon = handler.PlatformView.FindViewById<Android.Widget.ImageView>(searchIconId);
+				searchIcon.SetColorFilter(Android.Graphics.Color.Rgb(172, 157, 185), Android.Graphics.PorterDuff.Mode.SrcIn);
+			}
+
+#endif
+		});
+	}
+}
+
+
---
+++ FairyTalesDemo/Models/FairyTale.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace FairyTalesDemo.Models
+{
+	public class FairyTale
+	{
+		public string Name { get; set; }
+		public TimeSpan ReadTime { get; set; }
+		public string Image { get; set; }
+	}
+}
+
